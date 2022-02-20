0b3525eb0e3bfdd607945cef16c8b1590d229aab
Mete Balci <metebalci@gmail.com>
2022-02-20 20:14:19 +0100

    Update search.py

--- search.py
+++ search.py
@@ -4,10 +4,13 @@ def f(a, b, c):
     return (10 + a * (5 + b * (5 + (c * 5))))
 
 x = int(sys.argv[1])
-print("searching for: %d" % (x,))
-
-for a in range(1, 255):
-    for b in range(1, 255):
-        for c in range(1, 255):
-            if x == f(a, b, c):
-                print("%9d => a=%3d, b=%3d, c=%3d" % (x, a, b, c))
+if x % 8 <> 0:
+    print("ratio should be a multiple of 8")
+else:
+    print("searching for: %d" % (x,))
+    x = x / 8
+    for a in range(1, 255):
+        for b in range(1, 255):
+            for c in range(1, 255):
+                if x == f(a, b, c):
+                    print("%9d => a=%3d, b=%3d, c=%3d" % (x, a, b, c))
